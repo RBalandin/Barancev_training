617056f5a946d1298ae253caac09959cdcab7637
RBalandin <BalandinRoman3@yandex.ru>
2022-09-12 14:11:50 +0300

    Вынесена фикстура и декомпозирован код для теста с контактами

---
+++ test/add_new_contact.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+from fixture.Application import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Tot", middlename="Samiy", lastname="Chelovek", nickname="Pchel",
+                               title="CEO", company="Mashup", address="Lenin street", home="111", mobile="88005553535",
+                               work="+790990900090", fax="27438567", email="q23984765@mail.ru", email2="jefn",
+                               email3="sedjf", homepage="sodeifj", bday="12", bmonth="July", byear="1990", aday="17",
+                               amonth="February", ayear="1991", address2="awsrg", phone2="sardfg", notes="awer"))
+    app.session.logout()
+
+
+if __name__ == "__main__":
+    pytest.main()
